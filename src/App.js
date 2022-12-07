b2f1024b1560d16066865421daf2b29d17a7618c
Yvann <yvann.nadal@mail-esd.com>
2022-12-07 17:16:42 +0100

    install prismic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { PrismicRichText, useAllPrismicDocumentsByType } from '@prismicio/react'
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [recipes] = useAllPrismicDocumentsByType('recipes')
+  const [ingredients] = useAllPrismicDocumentsByType('ingredients')
+  const [drinks] = useAllPrismicDocumentsByType('drinks')
+  // const [fetch, setFetch] = useState(false)
+
+  useEffect(() => {
+    if (recipes && ingredients && drinks) {
+      console.log(recipes);
+      console.log("recipes");
+      console.log(ingredients);
+      console.log("ingredients");
+      console.log(drinks);
+      console.log("drinks");
+    }
+  }, [recipes, ingredients, drinks])
+
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div>
     </div>
-  );
+  )
 }
 
 export default App;
